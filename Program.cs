0b5cae0573bda0723009d32d40d270ef72cc7388
LifelessNerd <lukadevrij@gmail.com>
2022-02-18 14:22:31 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿for (int i = 0; i < 100; i++)
+{
+    if (!(i.ToString().Contains('4') || i.ToString().Contains('5')))
+    {
+        Console.WriteLine(i);
+    }
+    else { Console.WriteLine("\n"); }
+}
